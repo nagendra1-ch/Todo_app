f5e1f2760e7e2ec4e08a08c4d17de70f36f465e3
nagendra1-ch <chnagendra592@gmail.com>
2025-08-09 19:47:55 +0530

    first day

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Todo from './components/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [input, setInput] = useState('')
+    const [todos,setTodos]=useState([])
+    
+    const addTodo = (e) => {
+        e.preventDefault()  
+        setInput(e.target.value)
+        console.log(input)
+        let newtodos=[...todos,input]
+        setTodos(newtodos)
+        localStorage.setItem("todo",JSON.stringify(todos))
+        console.log(todos)
+        setInput('')
+    }
 
-  return (
-    <>
-    <h2 style={{"textAlign":'center' }}>Todo Application</h2>
-      <Todo/>
-      
-    </>
-  )
+    return (
+      <>
+        <h2 style={{ textAlign: 'center' }}>Todo Application</h2>
+        <form onSubmit={addTodo}>
+            <input
+                type="text"  placeholder='Enter Todo' value={input} onChange={(e)=>{setInput(e.target.value)}} />
+            <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      </>
+    )
 }
 
 export default App
+
